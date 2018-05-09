7c055b4c99e9c6db2614d96a96509fdc16dcb462
Tiago Chedraoui Silva <tiagochst@gmail.com>
2018-05-09 19:42:54 -0300

    Add apeglm option

--- GENAVi.R
+++ GENAVi.R
@@ -584,7 +584,7 @@ server <- function(input,output,session)
         }
       } else {
         if(input$lfc) {
-          tbl <-  as.data.frame(lfcShrink(res, coef = deaSelect)) ### adding apeglm option breaks results
+          tbl <-  as.data.frame(lfcShrink(res, type = "apeglm", coef = deaSelect)) ### adding apeglm option breaks results
         } else {
           if(lfcThreshold > 0){
             tbl <-  as.data.frame(results(res,
@@ -615,7 +615,7 @@ server <- function(input,output,session)
         dea <-  as.data.frame(results(res))
       } else {
         if(input$lfc) {
-          dea <-  as.data.frame(lfcShrink(res, coef = deaSelect)) ### adding apeglm option breaks results
+          dea <-  as.data.frame(lfcShrink(res, coef = deaSelect,type = "apeglm")) ### adding apeglm option breaks results
         } else {
           lfcThreshold <- input$log2FoldChange
           if(lfcThreshold > 0){
