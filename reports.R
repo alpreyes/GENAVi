09dc1a2fb0ac0cf923fcc67b83786af8dcf96c26
Tiago Chedraoui Silva <tiagochst@gmail.com>
2019-08-14 11:18:05 -0300

    Adding missing file

---
+++ reports.R
@@ -0,0 +1,123 @@
+
+# ---------------------------------
+# Creating reports with code
+# We will create for each section a HTML with the code to 
+# used in Genavi
+# - Required reports:
+#    - DEA
+#    - Normalization
+#    - PCA/t-sne
+#    - Volcano/Heatmap plots
+# ---------------------------------
+output$reportNorm <- downloadHandler(
+  
+  # For PDF output, change this to "report.pdf"
+  filename = "report_normalization_genavi.html",
+  content = function(file) {
+    # Copy the report file to a temporary directory before processing it, in
+    # case we don't have write permissions to the current working dir (which
+    # can happen when deployed).
+    tempReport <- file.path("normalization.Rmd")
+    #file.copy("/Users/tiago/Documents/GENAVi/report/report.Rmd", tempReport, overwrite = TRUE)
+    
+    # Set up parameters to pass to Rmd document
+    #params <- list(n = input$slider)
+    
+    # Knit the document, passing in the `params` list, and eval it in a
+    # child of the global environment (this isolates the code in the document
+    # from the code in this app).
+    rmarkdown::render(input = "normalization.Rmd", output_file = file)
+    
+  }
+)
+
+output$reportPCA <- downloadHandler(
+  
+  # For PDF output, change this to "report.pdf"
+  filename = "report_PCA_genavi.html",
+  content = function(file) {
+    
+    tbl.tab1 <- getTab1()
+    
+    # Columns 1 to 7: Genename  Geneid Chr   Start   End Strand Length 
+    res <- getEndGeneInfo(tbl.tab1)
+    ngene <- res$ngene
+    m <- tbl.tab1 %>% dplyr::select((res$ngene + 1):ncol(tbl.tab1)) %>% as.matrix  
+    rownames(m) <- tbl.tab1 %>% pull(1)
+    # Set up parameters to pass to Rmd document
+    params <- list(matrix = m)
+    
+    # Knit the document, passing in the `params` list, and eval it in a
+    # child of the global environment (this isolates the code in the document
+    # from the code in this app).
+    shiny::withProgress(value = 0,
+                        message = 'Rendering PCA report.',
+                        detail =  'This might take some minutes.',{
+                          rmarkdown::render(input = "report/pca.Rmd", 
+                                            params = params, 
+                                            output_file = file,
+                                            envir = new.env(parent = globalenv()))
+                        })
+    
+  }
+)
+
+output$reportDEA <- downloadHandler(
+  
+  # For PDF output, change this to "report.pdf"
+  filename = "report_DEA_genavi.html",
+  content = function(file) {
+    shiny::withProgress(value = 0,message = 'Rendering DEA report.',
+                        detail =  'This might take some minutes.',{
+                          
+                          metadata <- readMetaData()
+                          if(!is.null(readData())) all_cell_lines <- readData()
+                          res <- getEndGeneInfo(all_cell_lines)
+                          matrix <- res$data
+                          ngene <- res$ngene
+                          
+                          if("Symbol" %in% colnames(matrix)){
+                            genes <- matrix %>% pull("Symbol")
+                          } else if("Genename" %in% colnames(matrix))  {
+                            genes <- matrix %>% pull("Genename")
+                          } else {
+                            genes <- matrix %>% pull(1)
+                          }
+                          
+                          cts <- as.matrix(matrix[,(ngene + 1):ncol(matrix)])
+                          rownames(cts) <-  genes
+                          rownames(cts) <- matrix %>% pull(1)
+                          
+                          # Read aux values required for analysis (condition, covariates and reference value)
+                          cond <- isolate(input$condition)
+                          cov <- isolate(input$covariates)
+                          ref <-  isolate(input$reference)
+                          lfc <-  isolate(input$lfc)
+                          log2FoldChange <- isolate(input$log2FoldChange)
+                          lfc <-  isolate(input$lfc)
+                          deaSelect <-  isolate(input$deaSelect)
+                          padj <-  isolate(input$padj)
+                          
+                          # Set up parameters to pass to Rmd document
+                          params <- list( 
+                            log2FoldChange = log2FoldChange,
+                            padj = padj,
+                            metadata = metadata,
+                            deaSelect = deaSelect,
+                            lfc =  lfc,
+                            condition = cond,
+                            covariates = cov,
+                            raw_cts = cts,
+                            reference = ref)
+                          
+                          # Knit the document, passing in the `params` list, and eval it in a
+                          # child of the global environment (this isolates the code in the document
+                          # from the code in this app).
+                          rmarkdown::render(input = "report/DEA.Rmd", 
+                                            params = params, 
+                                            output_file = file,
+                                            envir = new.env(parent = globalenv()))
+                        })
+    
+  }
+)
